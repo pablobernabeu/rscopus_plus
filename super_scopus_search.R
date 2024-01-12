4f82019b82a84520d44739b2a49e817852b6e7c5
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-13 00:13:33 +0100

    Update super_scopus_search.R

--- super_scopus_search.R
+++ super_scopus_search.R
@@ -1,28 +1,23 @@
 
 
-# Run `rscopus::scopus_search` as many times as necessary based on the number of results, 
-# limited to limit of results per search allowed by my API quota (normally, 20).
+# Run `rscopus::scopus_search` as many times as necessary based on the number of results and the search quota.
 
-# Note. Before running the current function, the user must read in their Scopus API key 
-# confidentially (see https://cran.r-project.org/web/packages/rscopus/vignettes/api_key.html).
-# An error will be thrown if there's no Scopus API key registered.
+# Note. Before running the current function, the user must read in their Scopus API key confidentially 
+# (see https://cran.r-project.org/web/packages/rscopus/vignettes/api_key.html). An error appears if the key 
+# has not been read in.
 
-super_scopus_search = function(query, search_period) {
+super_scopus_search = function(query, search_period, quota) {
   
   require(dplyr)
   require(rscopus)
   
-  # Enable as many searches as necessary given the search quota
-  
-  my_quota = 20
-  
-  res = scopus_search(query = query, max_count = my_quota, count = my_quota, date = search_period)
+  res = scopus_search(query = query, max_count = quota, count = quota, date = search_period)
   
   safe_maximum = 5000  # protection from massive search
   
   total_results = 0 : res$total_results
   
-  chunks = total_results[seq(1, length(total_results), my_quota)]
+  chunks = total_results[seq(1, length(total_results), quota)]
   
   results = data.frame(author = as.character(), date = as.character(), 
                        title = as.character(), publication = as.character(), 
@@ -32,7 +27,7 @@ super_scopus_search = function(query, search_period) {
     
     for(i_chunk in 1 : n_distinct(chunks)) {
       
-      res = scopus_search(query = query, max_count = my_quota, count = my_quota, 
+      res = scopus_search(query = query, max_count = quota, count = quota, 
                           start = chunks[i_chunk], date = search_period)
       
       for(i_entry in 1 : length(res$entries)) {
