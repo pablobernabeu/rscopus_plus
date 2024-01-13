d59651c0fa02e064276abbaf6104926ebe1134e8
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-13 18:18:25 +0100

    Update super_scopus_search_additional_DOIs.R

--- super_scopus_search_additional_DOIs.R
+++ super_scopus_search_additional_DOIs.R
@@ -36,7 +36,7 @@ super_scopus_search_additional_DOIs =
       
       DOIs = super_scopus_search_DOIs(query, search_period, quota)
       
-      date_time = as.character(format(Sys.time(), "%Y-%m-%d %H%M"))
+      date_time = as.character(format(Sys.time(), '%Y-%m-%d %H%M'))
       
       if(isTRUE(save_date_time_file)) {
         fileConn = file(paste0(path, 'date and time of previous retrieval of DOIs.txt'))
