c7d4f5dd51b545961e21244c72e2c8a64a294a7f
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-13 15:16:48 +0100

    Create super_scopus_search_DOI.R

---
+++ super_scopus_search_DOI.R
@@ -0,0 +1,41 @@
+
+# Get DOIs from super_scopus_search()
+
+super_scopus_search_DOI = 
+  
+  function( query, 
+            search_period, 
+            quota,  # Scopus API quota 
+            path,   # directory path
+            save_date_time_file = FALSE  # save date and time in a text file
+  ) {
+    
+    require(rscopus)
+    
+    if(have_api_key()) {  # error if API key missing
+      
+      # Read in 'super_scopus_search' function
+      source('https://raw.githubusercontent.com/pablobernabeu/super_scopus_search/main/super_scopus_search.R')
+      
+      library(dplyr)
+      
+      results = super_scopus_search(query, search_period, quota)
+      
+      date_time = as.character(format(Sys.time(), "%d%m%Y %H%M"))
+      
+      if(isTRUE(save_date_time_file)) {
+        fileConn = file(paste0(path, 'date and time from last retrieval of DOIs.txt'))
+        writeLines(date_time, fileConn)
+        close(fileConn)
+      }
+      
+      DOIs = results[complete.cases(results$doi), 'doi']
+      
+      cat(DOIs, sep = '\n')
+      
+      file = paste0(path, 'DOIs, ', date_time, '.csv')
+      
+      write.csv(DOIs, file, row.names = FALSE)
+      
+    }
+  }
