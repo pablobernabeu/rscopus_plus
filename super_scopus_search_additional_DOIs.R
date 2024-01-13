fb56612c245187a9b71fb8e4e2115d483d297b0b
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-13 21:45:13 +0100

    Update super_scopus_search_additional_DOIs.R

--- super_scopus_search_additional_DOIs.R
+++ super_scopus_search_additional_DOIs.R
@@ -16,7 +16,12 @@ super_scopus_search_additional_DOIs =
     
     require(rscopus)
     
-    if(have_api_key()) {  # error if API key missing
+    if(!have_api_key()) {  # error if API key missing
+      
+      stop('The login key for the Scopus API has not been read in. Find out more at \n',
+           'https://cran.r-project.org/web/packages/rscopus/vignettes/api_key.html')
+      
+    } else {
       
       # Read in 'super_scopus_search_DOI' function
       source('https://raw.githubusercontent.com/pablobernabeu/super_scopus_search/main/super_scopus_search_DOIs.R')
