1a5c74915a6f61905f6ae8b20bcbb29c34574104
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-14 20:16:42 +0100

    Update super_scopus_search.R

--- super_scopus_search.R
+++ super_scopus_search.R
@@ -15,54 +15,52 @@ super_scopus_search =
     
     require(rscopus)
     
-    if(!have_api_key()) {  # error if API key missing
-      
+    # Error if API key missing
+    if(!have_api_key()) {
       stop('The login key for the Scopus API has not been read in. Find out more at \n',
            '  https://cran.r-project.org/web/packages/rscopus/vignettes/api_key.html')
-      
-    } else {
-      
-      require(dplyr)
-      
-      # Use tryCatch() to handle errors in scopus_search
-      res = tryCatch({
-        scopus_search( query = query, max_count = quota, 
-                       count = quota, date = search_period )
-      }, error = function(e) {
-        print(paste("Error in nested function 'scopus_search': ", e$message))  # Print error message to console
-      })
-      
-      total_results = 0 : res$total_results
-      
-      chunks = total_results[seq(1, length(total_results), quota)]
-      
-      results = data.frame(author = as.character(), date = as.character(), 
-                           title = as.character(), publication = as.character(), 
-                           doi = as.character())
-      
-      for(i_chunk in 1 : n_distinct(chunks)) {
-        
-        res = scopus_search(query = query, max_count = quota, count = quota, 
-                            start = chunks[i_chunk], date = search_period)
-        
-        for(i_entry in 1 : length(res$entries)) {
-          
-          author = res$entries[[i_entry]]$`dc:creator`
-          date = res$entries[[i_entry]]$`prism:coverDisplayDate`
-          title = res$entries[[i_entry]]$`dc:title`
-          publication = res$entries[[i_entry]]$`prism:publicationName`
-          doi = res$entries[[i_entry]]$`prism:doi`
-          
-          i_results = data.frame( author = ifelse(!is.null(author), author, NA), 
-                                  date = ifelse(!is.null(date), date, NA), 
-                                  title = ifelse(!is.null(title), title, NA), 
-                                  publication = ifelse(!is.null(publication), publication, NA), 
-                                  doi = ifelse(!is.null(doi), doi, NA) )
-          
-          results = rbind(results, i_results)
-          
-        }
-      }
-      return(results)
     }
+    
+    require(dplyr)
+    
+    # Use tryCatch() to handle errors in scopus_search
+    res = tryCatch({
+      scopus_search( query = query, max_count = quota, 
+                     count = quota, date = search_period )
+    }, error = function(e) {
+      print(paste("Error in nested function 'scopus_search': ", e$message))  # Print error message to console
+    })
+    
+    total_results = 0 : res$total_results
+    
+    chunks = total_results[seq(1, length(total_results), quota)]
+    
+    results = data.frame(author = as.character(), date = as.character(), 
+                         title = as.character(), publication = as.character(), 
+                         doi = as.character())
+    
+    for(i_chunk in 1 : n_distinct(chunks)) {
+      
+      res = scopus_search(query = query, max_count = quota, count = quota, 
+                          start = chunks[i_chunk], date = search_period)
+      
+      for(i_entry in 1 : length(res$entries)) {
+        
+        author = res$entries[[i_entry]]$`dc:creator`
+        date = res$entries[[i_entry]]$`prism:coverDisplayDate`
+        title = res$entries[[i_entry]]$`dc:title`
+        publication = res$entries[[i_entry]]$`prism:publicationName`
+        doi = res$entries[[i_entry]]$`prism:doi`
+        
+        i_results = data.frame( author = ifelse(!is.null(author), author, NA), 
+                                date = ifelse(!is.null(date), date, NA), 
+                                title = ifelse(!is.null(title), title, NA), 
+                                publication = ifelse(!is.null(publication), publication, NA), 
+                                doi = ifelse(!is.null(doi), doi, NA) )
+        
+        results = rbind(results, i_results)
+        
+      }
+    }
+    return(results)
   }
