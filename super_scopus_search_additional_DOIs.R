9efca5ba379b4fa0f1714282c24461028b0cabd0
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-27 17:41:58 +0100

    Update super_scopus_search_additional_DOIs.R

--- super_scopus_search_additional_DOIs.R
+++ super_scopus_search_additional_DOIs.R
@@ -42,8 +42,8 @@ scopus_search_plus_additional_DOIs =
     # Use tryCatch() to handle errors in scopus_search_plus_DOIs
     results = tryCatch({
       scopus_search_plus(query, search_period, quota, verbose = verbose)
-    }, error = function(e) {
-      print(paste("Error in nested function 'scopus_search_plus_DOIs': ", e$message))  # Print error message to console
+    }, error = function(e) {  # Print error message to console
+      print(paste("Error in nested function 'scopus_search_plus_DOIs': ", e$message))
     })
     
     DOIs = results[complete.cases(results$doi), 'doi']
