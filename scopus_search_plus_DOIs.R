8f9e636006fa6f159f099a5122eb9803ade3e3eb
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-01-27 18:02:08 +0100

    Update scopus_search_plus_DOIs.R

--- scopus_search_plus_DOIs.R
+++ scopus_search_plus_DOIs.R
@@ -27,7 +27,9 @@ scopus_search_plus_DOIs =
     
     # Use tryCatch() to handle errors in scopus_search_plus
     results = tryCatch({
+      
       scopus_search_plus(query, search_period, quota, verbose = verbose)
+      
     }, error = function(e) {  # Print error message to console
       print(paste("Error in nested function 'scopus_search_plus': ", e$message))
     })
