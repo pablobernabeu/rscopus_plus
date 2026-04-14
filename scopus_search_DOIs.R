99af9c162f2c007955059a2c74841945661977ac
Pablo Bernabeu <pcbernabeu@gmail.com>
2026-04-14 18:46:58 +0100

    Refactor scopus_search_DOIs function parameters
    Fix error handling and pagination robustness:
    
    return typed empty data frame from error handler instead of implicit NULL, preventing downstream crash on column access.

--- scopus_search_DOIs.R
+++ scopus_search_DOIs.R
@@ -1,52 +1,55 @@
 
-# Get DOIs from scopus_search_plus and save them in a CSV file. 
+# Get DOIs from scopus_search_plus and save them in a CSV file.
 
-scopus_search_DOIs = 
-  
-  function( query, 
-            search_period, 
-            quota = 5,                    # Scopus API quota 
+scopus_search_DOIs =
+
+  function( query,
+            search_period,
+            quota = 5,                    # Scopus API quota
             path,                         # directory path
             save_date_time_file = TRUE,   # save date and time in a text file
             verbose = TRUE,               # print progress in R console
             console_print_DOIs = FALSE    # print DOIs in R console
   ) {
-    
+
     require(rscopus)
-    
+
     # Error if API key missing
     if(!have_api_key()) {
       stop('The login key for the Scopus API has not been read in. Find out more at \n',
            '  https://cran.r-project.org/web/packages/rscopus/vignettes/api_key.html')
     }
-    
+
     # Read in 'scopus_search_plus' function
     source('https://raw.githubusercontent.com/pablobernabeu/rscopus_plus/main/scopus_search_plus.R')
-    
+
     require(dplyr)
-    
+
     # Use tryCatch() to handle errors in scopus_search_plus
     results = tryCatch({
-      
+
       scopus_search_plus(query, search_period, quota, verbose = verbose)
-      
+
     }, error = function(e) {  # Print error message to console
       print(paste("Error in nested function 'scopus_search_plus':", e$message))
+      return(data.frame(author = character(), date = character(),
+                        title = character(), publication = character(),
+                        doi = character()))
     })
-    
+
     DOIs = results[complete.cases(results$doi), 'doi']
-    
+
     if(isTRUE(console_print_DOIs)) cat(DOIs, '', sep = '\n')
-    
+
     date_time = as.character(format(Sys.time(), '%Y-%m-%d %H%M'))
-    
+
     if(isTRUE(save_date_time_file)) {
       fileConn = file(paste0(path, 'date and time of previous retrieval of DOIs.txt'))
       writeLines(date_time, fileConn)
       close(fileConn)
     }
-    
+
     file = paste0(path, 'DOIs, ', date_time, '.csv')
-    
+
     write.csv(unique(DOIs), file, row.names = FALSE)
   }
