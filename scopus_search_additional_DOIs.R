b81c4b576dfba942c820766ea30229f8db2b77e5
Pablo Bernabeu <pcbernabeu@gmail.com>
2026-04-14 19:03:37 +0100

    Fix error handling and source reference in scopus_search_additional_DOIs
    - Source scopus_search_plus.R directly instead of scopus_search_DOIs.R,
      since the function calls scopus_search_plus() not scopus_search_DOIs()
    - Return typed empty data frame from error handler instead of implicit NULL,
      preventing downstream crash on results$doi access

--- scopus_search_additional_DOIs.R
+++ scopus_search_additional_DOIs.R
@@ -1,75 +1,72 @@
 
-# Search for additional DOIs after running 
+# Search for additional DOIs after running
 # `scopus_search_DOIs(save_date_time_file = TRUE)`.
-#  Save the additional DOIs in a CSV file, and 
-# save all DOIS in another CSV file.
+# Save the additional DOIs in a CSV file, and
+# save all DOIs in another CSV file.
 
-scopus_search_additional_DOIs = 
-  
-  function( query, 
-            search_period, 
-            quota = 5,                    # Scopus API quota 
+scopus_search_additional_DOIs =
+
+  function( query,
+            search_period,
+            quota = 5,                    # Scopus API quota
             path,                         # directory path
             save_date_time_file = TRUE,   # save date and time in a text file
             verbose = TRUE,               # print progress in R console
             console_print_DOIs = FALSE    # print DOIs in R console
   ) {
-    
+
     require(rscopus)
-    
+
     # Error if API key missing
     if(!have_api_key()) {
       stop('The login key for the Scopus API has not been read in. Find out more at \n',
            '  https://cran.r-project.org/web/packages/rscopus/vignettes/api_key.html')
     }
-    
-    # Read in 'scopus_search_DOIs' function
-    source('https://raw.githubusercontent.com/pablobernabeu/rscopus_plus/main/scopus_search_DOIs.R')
-    
+
+    # Read in 'scopus_search_plus' function
+    source('https://raw.githubusercontent.com/pablobernabeu/rscopus_plus/main/scopus_search_plus.R')
+
     require(dplyr)
-    
+
     # Previous DOIs
-    
-    date_time_last_access = 
+    date_time_last_access =
       readLines(paste0(path, 'date and time of previous retrieval of DOIs.txt'))
-    
+
     last_file = paste0(path, 'DOIs, ', date_time_last_access, '.csv')
-    
+
     previous_DOIs = read.csv(last_file)
-    
+
     # Latest DOIs
-    
-    # Use tryCatch() to handle errors in scopus_search_DOIs
     results = tryCatch({
-      
       scopus_search_plus(query, search_period, quota, verbose = verbose)
-      
-    }, error = function(e) {  # Print error message to console
-      print(paste("Error in nested function 'scopus_search_DOIs':", e$message))
+    }, error = function(e) {
+      message("Error in nested function 'scopus_search_plus': ", e$message)
+      return(data.frame(author = character(), date = character(),
+                        title = character(), publication = character(),
+                        doi = character()))
     })
-    
+
     DOIs = results[complete.cases(results$doi), 'doi']
-    
+
     date_time = as.character(format(Sys.time(), '%Y-%m-%d %H%M'))
-    
+
     file = paste0(path, 'DOIs, ', date_time, '.csv')
-    
+
     write.csv(unique(DOIs), file, row.names = FALSE)
-    
+
     # Find the DOIs that were not in the previous retrieval
-    
-    additional_DOIs = DOIs[!DOIs %in% previous_DOIs$x] 
-    
+    additional_DOIs = DOIs[!DOIs %in% previous_DOIs$x]
+
     unique(additional_DOIs) %>%
-      write.csv(paste0(path, 'additional DOIs, ', 
-                       date_time, '.csv'), 
+      write.csv(paste0(path, 'additional DOIs, ',
+                       date_time, '.csv'),
                 row.names = FALSE)
-    
+
     if(isTRUE(save_date_time_file)) {
       fileConn = file(paste0(path, 'date and time of previous retrieval of DOIs.txt'))
       writeLines(date_time, fileConn)
       close(fileConn)
     }
-    
+
     if(isTRUE(console_print_DOIs)) cat(additional_DOIs, '', sep = '\n')
   }
